f661a94f1da4f03f39331b3dbe1d445cd7cf5466
othman853 <anuar.yasser@gmail.com>
2017-09-05 12:50:48 -0300

    Implements component tess for SystemLoggerProvider

--- src/test/java/component/SystemLoggerProviderTest.java
+++ src/test/java/component/SystemLoggerProviderTest.java
@@ -1,41 +1,62 @@
 package component;
 
 import com.othman853.fluentlogs.SystemLoggerProvider;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SystemLoggerProviderTest {
+
+    private final ByteArrayOutputStream standardOutput = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream standardErrorOutput = new ByteArrayOutputStream();
 
     private SystemLoggerProvider provider;
 
     @Before
     public void setup() {
+        System.setOut(new PrintStream(standardOutput));
+        System.setErr(new PrintStream(standardErrorOutput));
+
         provider = new SystemLoggerProvider();
+    }
+
+    @After
+    public void cleanup() {
+        System.setOut(null);
+        System.setErr(null);
     }
 
     @Test
     public void should_log_info_to_standard_output() {
         provider.info().action("banana").log();
-        fail();
+
+        assertThat(standardOutput.toString(), is("action=\"banana\"\n"));
     }
 
     @Test
     public void should_log_debug_to_standard_output() {
         provider.debug().action("banana").log();
-        fail();
+
+        assertThat(standardOutput.toString(), is("action=\"banana\"\n"));
     }
 
     @Test
     public void should_log_warning_to_standard_output() {
         provider.warning().action("banana").log();
-        fail();
+
+        assertThat(standardOutput.toString(), is("action=\"banana\"\n"));
     }
 
     @Test
     public void should_log_error_to_standard_error() {
         provider.error().action("banana").log();
-        fail();
+
+        assertThat(standardErrorOutput.toString(), is("action=\"banana\"\n"));
     }
 }
