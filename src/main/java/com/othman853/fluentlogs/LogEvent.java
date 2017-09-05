ab3dcecb82dd5efecfe894affa28340d6a1a5ac2
othman853 <anuar.yasser@gmail.com>
2017-09-05 12:21:43 -0300

    Brainstorm

--- src/main/java/com/othman853/fluentlogs/LogEvent.java
+++ src/main/java/com/othman853/fluentlogs/LogEvent.java
@@ -2,11 +2,16 @@ package com.othman853.fluentlogs;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Optional;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @RequiredArgsConstructor
 public class LogEvent {
+
     private final LogMessage message;
+
     private final Consumer<String> logger;
 
     public void log() {
@@ -20,6 +25,34 @@ public class LogEvent {
 
     public LogEvent description(String value) {
         message.add("description", value);
+        return this;
+    }
+
+    public LogEvent field(String name, String value) {
+       return this;
+    }
+
+    public LogEvent field(String name, Supplier<String> value) {
+        return this;
+    }
+
+    public LogEvent field(Loggable loggable) {
+        return this;
+    }
+
+    public <T> LogEvent optional(String name, Optional<T> optional) {
+       return this;
+    }
+
+    public <T> LogEvent ifPresent(Optional<T> optional, BiConsumer<LogEvent, T> whenPresent) {
+       return this;
+    }
+
+    public <T> LogEvent nullable(String name, T nullable) {
+        return this;
+    }
+
+    public <T> LogEvent ifNotNull(T nullable, BiConsumer<LogEvent, T> whenNotNull) {
         return this;
     }
 }
